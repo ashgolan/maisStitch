5ea30770202a2d09ab4532bc3ae803061094fe4e
Alaa Shaalan <alaa.t.shaalan@gmail.com>
2024-04-11 16:59:14 +0300

    add js cookie

--- client/src/utils/Api.jsx
+++ client/src/utils/Api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 let url = "http://localhost:5000";
 
 if (process.env.NODE_ENV === "production") {
-  url = "https://khaled-khateeb-server.onrender.com";
+  url = "https://maisstitch-server.onrender.com";
 }
 
 const Api = axios.create({
