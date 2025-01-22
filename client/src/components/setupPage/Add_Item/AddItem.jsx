8b4b9ff1e68145833aa9b89c61fc842d1830ad21
ashgolan <alaa.t.shaalan@gmail.com>
2025-01-22 04:07:23 +0200

    add items

--- client/src/components/setupPage/Add_Item/AddItem.jsx
+++ client/src/components/setupPage/Add_Item/AddItem.jsx
@@ -209,8 +209,9 @@ export default function AddItem({
     { value: "2", label: "طاره" },
     { value: "3", label: "محرمه" },
     { value: "4", label: "لوغو" },
-    { value: "5", label: "آخر" },
+    { value: "5", label: "تصليح" },
     { value: "6", label: "فوطه + محرمه" },
+    { value: "6", label: "فوطه + طاره" },
     { value: "7", label: "طاره + محرمه" },
     { value: "8", label: "فوطه + طاره + محرمه" },
   ].map((item) => {
--- client/src/components/setupPage/Items_Table/ItemsTable.jsx
+++ client/src/components/setupPage/Items_Table/ItemsTable.jsx
@@ -104,8 +104,9 @@ export default function ItemsTable({
     { value: "2", label: "طاره" },
     { value: "3", label: "محرمه" },
     { value: "4", label: "لوغو" },
-    { value: "5", label: "آخر" },
+    { value: "5", label: "تصليح" },
     { value: "6", label: "فوطه + محرمه" },
+    { value: "6", label: "فوطه + طاره" },
     { value: "7", label: "طاره + محرمه" },
     { value: "8", label: "فوطه + طاره + محرمه" },
   ].map((item) => {
